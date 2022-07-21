e17e45fef2b3f88533b57294ed9c694bef22056f
RenatoRios1 <renatoarios1@gmail.com>
2022-07-21 09:53:22 -0500

    first

---
+++ code-connected-server/src/main/java/com/team200/codeconnectedserver/CodeConnectedServerApplication.java
@@ -0,0 +1,13 @@
+package com.team200.codeconnectedserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeConnectedServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeConnectedServerApplication.class, args);
+	}
+
+}
---
+++ code-connected-server/src/test/java/com/team200/codeconnectedserver/CodeConnectedServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team200.codeconnectedserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeConnectedServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
