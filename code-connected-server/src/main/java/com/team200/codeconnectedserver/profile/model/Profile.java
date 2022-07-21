3c27797930675f07051148a0a3658dd1a261b71c
davidupnext <dadeleke31@gmail.com>
2022-07-21 14:11:05 -0400

    security auth & profile model

--- code-connected-server/src/main/java/com/team200/codeconnectedserver/profile/model/Profile.java
+++ code-connected-server/src/main/java/com/team200/codeconnectedserver/profile/model/Profile.java
@@ -1,4 +1,28 @@
 package com.team200.codeconnectedserver.profile.model;
 
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@ToString
+@NoArgsConstructor
+@RequiredArgsConstructor
 public class Profile {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NonNull
+    private String firstName;
+    @NonNull
+    private String lastName;
+    @NonNull
+    List<Education> school;
+    @NonNull
+    List<Job> experience;
+
 }
