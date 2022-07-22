5516b3258cb207557fe38a259b6cbcf67a629dc0
Boyce007 <diboyce21@students.desu.edu>
2022-07-22 11:13:52 -0400

    Updates

--- code-connected-server/src/main/java/com/team200/codeconnectedserver/domain/education/repo/EducationRepo.java
+++
@@ -1,7 +0,0 @@
-package com.team200.codeconnectedserver.domain.education.repo;
-
-import com.team200.codeconnectedserver.domain.education.model.Education;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EducationRepo extends JpaRepository<Education,Long> {
-}
