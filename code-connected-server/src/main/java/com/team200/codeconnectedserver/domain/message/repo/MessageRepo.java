6a573212679fb8192360d0f9fe5ff3f8c969b360
Tamajong Nchukwi <tjnc@Tamajongs-MacBook-Pro-2.local>
2022-07-23 21:14:28 -0400

    updates to message and message service test complete

--- code-connected-server/src/main/java/com/team200/codeconnectedserver/domain/message/repo/MessageRepo.java
+++ code-connected-server/src/main/java/com/team200/codeconnectedserver/domain/message/repo/MessageRepo.java
@@ -4,9 +4,10 @@ import com.team200.codeconnectedserver.domain.chat.model.Chat;
 import com.team200.codeconnectedserver.domain.message.model.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MessageRepo extends JpaRepository<Message, Long> {
     Iterable<Message> findBySenderId(Long id);
-    Iterable<Message> findByChat(Chat chat);
+    List<Message> findByChat(Chat chat);
 }
