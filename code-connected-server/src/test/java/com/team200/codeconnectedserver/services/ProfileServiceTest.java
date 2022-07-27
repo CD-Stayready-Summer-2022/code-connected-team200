58096a3ac5452edf92f91d4dc7b23412ccc72025
davidupnext <dadeleke31@gmail.com>
2022-07-27 15:07:49 -0400

    Service Tests

--- code-connected-server/src/test/java/com/team200/codeconnectedserver/services/ProfileServiceTest.java
+++ code-connected-server/src/test/java/com/team200/codeconnectedserver/services/ProfileServiceTest.java
@@ -1,14 +1,12 @@
 package com.team200.codeconnectedserver.services;
 
 import com.team200.codeconnectedserver.domain.blogpost.model.BlogPost;
-import com.team200.codeconnectedserver.domain.core.Exceptions.ResourceCreationException;
 import com.team200.codeconnectedserver.domain.education.model.Education;
 import com.team200.codeconnectedserver.domain.exceptions.ResourceCreationException;
 import com.team200.codeconnectedserver.domain.job.model.Job;
 import com.team200.codeconnectedserver.domain.profile.model.Profile;
 import com.team200.codeconnectedserver.domain.profile.repo.ProfileRepo;
 import com.team200.codeconnectedserver.domain.profile.service.ProfileService;
-import com.team200.codeconnectedserver.exceptions.ProfileCreationException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -40,30 +38,61 @@ public class ProfileServiceTest {
     private List<Profile> mockConnectionList;
     private List<Education> mockEducationList;
     private List<Job> mockExperience;
-    private List<Profile>mockfollowers;
-    private List<Profile>mockfollowing;
+    private List<Profile> mockfollowers;
+    private List<Profile> mockfollowing;
     List<BlogPost> mockposts;
 
     @BeforeEach
-    public void setUp01(){
+    public void setUp01() {
         mockConnectionList = new ArrayList<>();
         mockfollowers = new ArrayList<>();
         mockfollowing = new ArrayList<>();
         mockposts = new ArrayList<>();
 
-        Profile mockProfile01 = new  Profile("firstName","lastName",mockEducationList,mockExperience,"email","password","description",mockConnectionList,mockfollowers,mockfollowing,mockposts);
-        Profile savedProfile01 = new  Profile("firstName","lastName",mockEducationList,mockExperience,"email","password","description",mockConnectionList,mockfollowers,mockfollowing,mockposts);
+        Profile mockProfile01 = new Profile("firstName", "lastName", mockEducationList, mockExperience, "email", "password", "description", mockConnectionList, mockfollowers, mockfollowing, mockposts);
+        Profile savedProfile01 = new Profile("firstName", "lastName", mockEducationList, mockExperience, "email", "password", "description", mockConnectionList, mockfollowers, mockfollowing, mockposts);
         savedProfile01.setId(1L);
 
-        Profile savedProfile02 = new  Profile("firstName2","lastName2",mockEducationList,mockExperience,"email2","password2","description2",mockConnectionList,mockfollowers,mockfollowing,mockposts);
-        }
-        @Test
-        @DisplayName("Create Person - Success")
-        public void createProfileTest01() throws ResourceCreationException {
-            BDDMockito.doReturn(Optional.empty()).when(profileRepo).findById(ArgumentMatchers.any());
-            BDDMockito.doReturn(savedProfile01).when(profileRepo).save(mockProfile01);
-            Profile profile = profileService.create(mockProfile01);
-            Assertions.assertNotNull(profile.getId());
-        }
+        Profile savedProfile02 = new Profile("firstName2", "lastName", mockEducationList, mockExperience, "email2", "password2", "description2", mockConnectionList, mockfollowers, mockfollowing, mockposts);
+    }
 
+    @Test
+    @DisplayName("Create Profile - Success")
+    public void createProfileTest01() throws ResourceCreationException {
+        BDDMockito.doReturn(Optional.empty()).when(profileRepo).findById(ArgumentMatchers.any());
+        BDDMockito.doReturn(savedProfile01).when(profileRepo).save(mockProfile01);
+        Profile profile = profileService.create(mockProfile01);
+        Assertions.assertNotNull(profile.getId());
+    }
+
+    @Test
+    @DisplayName("Create Profile - fail")
+    public void createProfileTest02() throws ResourceCreationException {
+        BDDMockito.doReturn(Optional.of(savedProfile01)).when(profileRepo).findById(ArgumentMatchers.any());
+        BDDMockito.doReturn(savedProfile01).when(profileRepo).save(mockProfile01);
+        Assertions.assertThrows(ResourceCreationException.class, () -> {
+            profileService.create(mockProfile01);
+        });
+    }
+
+    @Test
+    @DisplayName("Get By Id")
+    public void getPersonByIdTest01() {
+        BDDMockito.doReturn(Optional.of(savedProfile01)).when(profileRepo).findById(1l);
+        Profile profile = profileService.getById(1l);
+        Assertions.assertNotNull(profile);
+    }
+
+    @Test
+    @DisplayName("Get By Last name - success")
+    public void getByLastNameTest01() {
+        List<Profile> profiles = new ArrayList<>();
+        profiles.add(savedProfile01);
+        profiles.add(savedProfile02);
+        BDDMockito.doReturn(profiles).when(profileRepo).findByLastName("Hook");
+        List<Profile> actualPeople = profileService.getByLastName("Hook");
+        Integer expected = 2;
+        Integer actual = actualPeople.size();
+        Assertions.assertEquals(expected, actual);
+    }
 }
